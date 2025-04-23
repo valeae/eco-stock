1f13b05c88accfb1ae0544764f46a5ffa9368af4
Valentina Aguirre <vaescobar95.ve@gmail.com>
2025-04-22 20:24:15 -0500

    Home

--- eco-stock-frontend/tailwind.config.js
+++ eco-stock-frontend/tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './pages/**/*.{ts,tsx}',
-      './components/**/*.{ts,tsx}',
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#BEC6A0',
+        accent: '#708871',
+        heading: '#2F3645',
+        muted: '#D9D9D9',
+      },
+      scrollBehavior: {
+        smooth: "smooth",
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
