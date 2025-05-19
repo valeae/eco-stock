8497f11e746fe7b7feefd2fb2be9d51541f57b73
Emanuel <manopalacio47@gmail.com>
2025-05-19 08:41:49 -0500

    se conecto la base de datos

--- eco-stock-backend/main_app/settings.py
+++ eco-stock-backend/main_app/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecostock_v2',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
