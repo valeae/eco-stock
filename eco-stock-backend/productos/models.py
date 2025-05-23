99d6aaacea771f418de624ae73ea24c473fe426b
Emanuel <manopalacio47@gmail.com>
2025-05-23 09:25:18 -0500

    actualizacion de modelo en productos

--- eco-stock-backend/productos/models.py
+++ eco-stock-backend/productos/models.py
@@ -6,6 +6,10 @@ class categoria(models.Model):
     idcategoria = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=255,null=False)
     descripcion = models.CharField(null=False)
+    tipo = models.CharField(null=False)
+    vida_util = models.CharField(null=False)
+    presentacion = models.CharField(null=False)
+
 
 class unidades_medida(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -16,5 +20,6 @@ class producto(models.Model):
     idproducto = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=255,null=False)
     descripcion = models.CharField(null=False)
+    lote = models.CharField(null=False)
     idcategoria = models.ForeignKey(categoria.idcategoria)
     unidad_medida_id = models.ForeignKey(unidades_medida.id)
