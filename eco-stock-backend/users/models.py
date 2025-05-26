b86fab95c0e78e0697cc1b964eb43e987bf12eb2
Emanuel <manopalacio47@gmail.com>
2025-05-26 07:59:17 -0500

    se creó el modelo de users

--- eco-stock-backend/users/models.py
+++ eco-stock-backend/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Rol(models.model):
+    nombre = models.CharField(null=False)
+
+class Usuario(models.Model):
+    idusuario = models.BigIntegerField(primary_key=True)
+    nombre = models.CharField(null=False)
+    correo_electronico = models.CharField(null=False)
+    contraseña = models.CharField(null=False)
+    idrol = models.ForeignKey(Rol.pk)
